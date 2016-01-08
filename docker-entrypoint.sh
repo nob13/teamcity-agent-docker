6a56aec2faafcb8318e204ec2bd2c5ad37d36ebb
Norbert Schultz <norbert.schultz@reactivecore.de>
2016-01-08 16:16:30 +0100

    Merge fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,22 +4,18 @@ if [ -z "$TEAMCITY_SERVER" ]; then
     exit 1
 fi
 
-if [ ! -d "$AGENT_DIR/bin" ]; then
-    echo "$AGENT_DIR doesn't exist pulling build-agent from server $TEAMCITY_SERVER";
-    let waiting=0
-    until curl -s -f -I -X GET $TEAMCITY_SERVER/update/buildAgent.zip; do
-        let waiting+=3
-        sleep 3
-        if [ $waiting -eq 120 ]; then
-            echo "Teamcity server did not respond within 120 seconds"...
-            exit 42
-        fi
-    done
-    wget $TEAMCITY_SERVER/update/buildAgent.zip && unzip -d $AGENT_DIR buildAgent.zip && rm buildAgent.zip
+if [ ! -f "$AGENT_DIR/installed" ]; then
+    echo "$AGENT_DIR/installed doesn't exist pulling build-agent from server $TEAMCITY_SERVER";
+    wget $TEAMCITY_SERVER/update/buildAgent.zip
+    unzip -d $AGENT_DIR buildAgent.zip
+    rm buildAgent.zip
     chmod +x $AGENT_DIR/bin/agent.sh
     echo "serverUrl=${TEAMCITY_SERVER}" > $AGENT_DIR/conf/buildAgent.properties
+    echo "workDir=/data/work" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "tempDir=/data/temp" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "systemDir=../system" >> $AGENT_DIR/conf/buildAgent.properties
+    touch $AGENT_DIR/installed
 fi
-
 echo "Starting buildagent..."
 chown -R teamcity:teamcity /opt/buildAgent
 
